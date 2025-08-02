53c28addad611495cdb3356cc4237f306196d63c
ant-bar <barr.star77@gmail.com>
2025-08-02 09:38:57 -0500

    analysis python script using incremental cell execution in VsCode (# %%)

--- pandas-practice/analysis.py
+++ pandas-practice/analysis.py
@@ -1,9 +1,33 @@
 # %%
 import pandas as pd
+import numpy as np
+
 df = pd.read_csv('hotel-booking-data.txt', delimiter = '\t')
 df.head()
 # %%
 df.dropna()
 # %%
 df.describe()
+# %%
+df.value_counts('Company')
+
+# %%
+df2 = df.value_counts('Company')
+df2.plot()
+# %%
+mask = df['Room number'].isna()
+
+df['Booking Method'] = np.where(mask, df['Date'], np.nan)
+df
+# %%
+df['Booking Method'].fillna(method = 'bfill', inplace=True)
+df
+# %%
+df.dropna(inplace=True)
+df
+# %%
+df2 = df.value_counts('Booking Method')
+df2.plot()
+# %%
+df.tail()
 # %%
